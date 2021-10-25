9b9631e9afb95a2196ffadf6a4c6ca79082eb5f5
Kathleen Wine <katie.wine2@gmail.com>
2021-10-24 20:00:12 -0400

    adding inquirer questions

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (name)
+VALUES ("Marketing"),
+("Technology"),
+("Finance"),
+("Fundraising"),
+("Service Delivery")
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Media Advertising", 60000, 1),
+("Software Engineer", 100000, 2),
+("Product Owner", 110000, 2),
+("UX Designer", 90000, 2),
+("Market Analyst", 75000, 3),
+("Investment Analyst", 104000, 3),
+("PDG Rep", 93000, 4),
+("Fundraiser", 107000, 4),
+("Phone Rep", 56000, 5),
+("Support Rep", 82000, 5)
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 4),
+("Steven", "Hendricks", 3),
+("Julia", "Jacobs", 6),
+("Michael", "Michaud", 10),
+("Alex", "Lane", 2),
+("Catherine", "Hamilton", 4),
+("Jerid", "Hayden", 7),
+("Jeremiah", "Reid", 5),
+("Richard", "Walker", 2)
---
+++ index.js
@@ -0,0 +1,90 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "password",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+const initialQuestions = [
+  {
+    type: "list",
+    name: "task",
+    message: "Enter the manager's name:",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all empoloyees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee",
+    ],
+  },
+];
+
+const addDepartment = [
+  {
+    type: "input",
+    name: "addDepartment",
+    message: "Enter the name of the department you would like to add:"
+  }
+];
+
+const addRole = [
+  {
+    type: "input",
+    name: "roleName",
+    message: "Enter the name of the role you would like to add:"
+  },
+  {
+    type: "input",
+    name: "salary",
+    message: "Enter the salary for the role:"
+  },
+  // how to add a dynamic list for existing departments
+  {
+    type: "input",
+    name: "department",
+    message: "Enter a department for the role:"
+  }
+];
+
+const addEmployee = [
+  {
+    type: "input",
+    name: "firstName",
+    message: "Enter the new employee's first name:"
+  },
+  {
+    type: "input",
+    name: "lastName",
+    message: "Enter the new employee's last name:"
+  },
+  // add a dynamic list for roles available
+  {
+    type: "input",
+    name: "role",
+    message: "Select a role for the new employee:"
+  },
+  {
+    type: "input",
+    name: "manager",
+    message: "Select a manager for the new employee:"
+  }
+];
+
+const updateEmployee = [
+  {
+    type: "input",
+    name: "employee",
+    message: "Select an employee to update:"
+  }
+]
